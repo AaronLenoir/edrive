02b138a140e7673a095b08bc399c47a9fcef15f5
AaronLenoir <lenoir.aaron@gmail.com>
2020-02-15 00:21:05 +0100

    Move trip overview to component

--- html/js/app.js
+++ html/js/app.js
@@ -1,5 +1,33 @@
 let url = 'https://spreadsheets.google.com/feeds/cells/16hJ_67ox89L3DuVuc8KWAGMYFwb58rVP2fjCkc2AoGE/1/public/full?alt=json';
 
+Vue.component('trip-overview', {
+  props: ['rows'],
+  template: `
+  <table class="pure-table">
+    <thead>
+      <tr v-for="row in rows.slice(0,1)">
+        <th v-if="row.cells[0].row == 1" v-for="cell in row.cells">
+          {{ cell.inputValue }}
+        </th>
+        <td v-if="row.cells[0].row > 1" v-for="cell in row.cells">
+          {{ cell.inputValue }}
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in rows.slice(1)">
+        <th v-if="row.cells[0].row == 1" v-for="cell in row.cells">
+          {{ cell.inputValue }}
+        </th>
+        <td v-if="row.cells[0].row > 1" v-for="cell in row.cells">
+          {{ cell.inputValue }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  `
+});
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -22,7 +50,7 @@ var app = new Vue({
   methods: {
     getRows: function () {
       // group by row
-      let rows = app.data.feed.entry.reduce((acc, reducer) => {
+        let rows = app.data.feed.entry.reduce((acc, reducer) => {
         let row = acc[reducer.gs$cell.row - 1] || { row: reducer.gs$cell.row, cells: [] };
         row.cells.push(reducer.gs$cell);
         acc[reducer.gs$cell.row - 1] = row;

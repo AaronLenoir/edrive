d3d7f974c36c767504d561e36895f702d656de2b
AaronLenoir <lenoir.aaron@gmail.com>
2020-03-03 22:00:28 +0100

    Change representation of some data

--- html/js/app.js
+++ html/js/app.js
@@ -19,10 +19,10 @@ Vue.component('trip-overview', {
       <th>Airco</th>
       <th>Highway</th>
       <th>Rain</th>
-      <th>Trip distance (km)</th>
-      <th>Elapsed time (min)</th>
-      <th>Avg Speed (km/h)</th>
-      <th>Avg Temp</th>
+      <th>Trip distance</th>
+      <th>Elapsed time</th>
+      <th>Average Speed</th>
+      <th>Average Temp</th>
       <th>Used battery</th>
       <th>Battery / 100 km</th>
       <th>kWh / 100 km</th>
@@ -30,12 +30,12 @@ Vue.component('trip-overview', {
     </thead>
     <tbody>
       <tr v-for="trip in trips.items">
-        <td>{{ trip.startDateTime.toDateString() }}</td>
-        <td>{{ trip.startDateTime.toTimeString() }}</td>
+        <td>{{ trip.startDateTime.getFullYear() }}-{{ trip.startDateTime.getMonth().toString().padStart(2, 0) }}-{{ trip.startDateTime.getDay().toString().padStart(2, 0) }}</td>
+        <td>{{ trip.startDateTime.getHours().toString().padStart(2, 0) }}:{{ trip.startDateTime.getMinutes().toString().padStart(2, 0) }}</td>
         <td>{{ trip.startKm.toFixed() }}</td>
         <td>{{ (trip.startCharge * 100).toFixed() }} %</td>
         <td>{{ trip.startTemp.toFixed() }} &deg;C</td>
-        <td>{{ trip.endDateTime.toTimeString() }}</td>
+        <td>{{ trip.endDateTime.getHours().toString().padStart(2, 0) }}:{{ trip.endDateTime.getMinutes().toString().padStart(2, 0) }}</td>
         <td>{{ trip.endKm.toFixed() }}</td>
         <td>{{ (trip.endCharge * 100).toFixed() }} %</td>
         <td>{{ trip.endTemp.toFixed() }} &deg;C</td>
@@ -45,10 +45,10 @@ Vue.component('trip-overview', {
         <td>{{ trip.airco ? "Yes" : "No" }}</td>
         <td>{{ trip.highway ? "Yes" : "No" }}</td>
         <td>{{ trip.rain ? "Yes" : "No" }}</td>
-        <td>{{ trip.distance }}</td>
-        <td>{{ trip.elapsedTime }}</td>
-        <td>{{ trip.averageSpeed.toFixed() }}</td>
-        <td>{{ trip.averageTemp }} &deg;C</td>
+        <td>{{ trip.distance }} km</td>
+        <td>{{ Math.floor(trip.elapsedTime / 60).toString().padStart(2, 0) }}:{{ Math.floor(trip.elapsedTime % 60).toFixed().toString().padStart(2, 0) }}</td>
+        <td>{{ trip.averageSpeed.toFixed() }} km/h</td>
+        <td>{{ trip.averageTemp.toFixed(2) }} &deg;C</td>
         <td>{{ (trip.usedBattery *100).toFixed() }} %</td>
         <td>{{ (trip.batteryPer100Km * 100).toFixed() }} %</td>
         <td>{{ (trip.kWhPer100Km * 100).toFixed() }}</td>
